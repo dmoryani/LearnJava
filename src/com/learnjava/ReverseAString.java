e5c82d58e23669aabb8e474ee0ec1450174ef193
Divya <dmoryani@gmail.com>
2020-01-19 12:56:26 +0530

    Adding a program to Reverse a String

---
+++ src/com/learnjava/ReverseAString.java
@@ -0,0 +1,28 @@
+package com.learnjava;
+
+
+/*How to Reverse a String in Java */
+
+/* 1. We can either use StringBuilder and use the inbuilt function or use the for loop and reverse logic.. */
+
+
+public class ReverseAString {
+	
+	public static void main(String[] args) {
+		
+		String s = "Reverseme";
+		String rev = "";
+		// 1. using StringBuilder
+		/*StringBuilder sb = new StringBuilder(s);
+		
+		System.out.println(sb.reverse());*/
+		
+		for(int i = s.length()-1;i>=0;i--){
+			
+			rev = rev+ s.charAt(i);
+		}
+		System.out.println(rev);
+		
+	}
+
+}
