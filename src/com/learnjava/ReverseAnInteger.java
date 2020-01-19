af2cb01b0f84aacda89ee1dfa1d5b15cacc66cbe
Divya <dmoryani@gmail.com>
2020-01-19 15:40:36 +0530

    Program 3: Reverse an Integer

---
+++ src/com/learnjava/ReverseAnInteger.java
@@ -0,0 +1,23 @@
+package com.learnjava;
+
+public class ReverseAnInteger {
+	
+	public static void main(String[] args) {
+		
+		int num =123458;
+		int rev =0;
+		
+		while(num!=0){
+			
+			rev = rev*10+num%10;
+			num= num/10;
+		
+	}
+		System.out.println(rev);
+	
+		
+		
+		
+	}
+
+}
